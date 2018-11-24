e870e460dfcac670d3a5c67452da61133c431b43
Albano Alice <alice.albano@albano_alice@yahoo.fr>
2018-11-24 17:05:15 +0100

    [REFACTOR] put developper cost in a variable

--- hire.js
+++ hire.js
@@ -1,11 +1,21 @@
 var developperNumber = 0;
+var developperCost = 5;
 
 function hireDevelopper() {
-    if (prospectRessources < 5) {
+    if (prospectRessources < developperCost) {
         return
     }
     developperNumber++;
     var span = document.getElementById("developper-number");
     span.textContent = developperNumber;
-    addProspectRessource(-5);
+    addProspectRessource(-developperCost);
 }
+
+function setHireDevelopperButtonText(text) {
+    var button = document.getElementById("hire-developper-button");
+    button.textContent = text;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setHireDevelopperButtonText("Hire (" + developperCost + ")");
+}, false);
