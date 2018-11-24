303fc0e7b4ef3b3703055510a492d257d506e3a5
Albano Alice <alice.albano@albano_alice@yahoo.fr>
2018-11-24 19:08:15 +0100

    [FIX] Initialize money at loading time

--- money.js
+++ money.js
@@ -1,8 +1,12 @@
 class Money extends Resource {
     constructor(tag) {
         super(tag);
-        this.number = 100000;
+        this.number = 10000;
     }
 }
 
 var money = new Money("money-money-money");
+
+document.addEventListener("DOMContentLoaded", function() {
+    money.updateUI();
+}, false);
